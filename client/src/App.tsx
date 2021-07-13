eb472b6e61dc2476f488b66df5706ee4dcf1cbea
Bernard Tison <bernard.tison@gmail.com>
2021-07-13 12:46:04 +0200

    Set up ApolloClient

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFoundView from './views/NotFoundView';
 import HomeView from './views/HomeView';
 import Nav from './components/Nav';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: getGraphqlHost(),
+  cache: new InMemoryCache()
+});
+console.log('connecting to graphql service at', getGraphqlHost());
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col h-screen justify-between">
-        <Nav />
-        <main className="mb-auto h-10">
-          <Switch>
-            <Route exact path={'/'} component={HomeView} />
-            <Route component={NotFoundView} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex flex-col h-screen justify-between">
+          <Nav />
+          <main className="mb-auto h-10">
+            <Switch>
+              <Route exact path={'/'} component={HomeView} />
+              <Route component={NotFoundView} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
+}
+
+function getGraphqlHost(): string {
+  return window.location.protocol + '//' + window.location.host + '/meters-graphql/graphql';
 }
 
 export default App;
